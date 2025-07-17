623a85b4cfd10bee04b1000cb2a849f4968e6055
lby9906 <lby9906@naver.com>
2025-07-17 09:16:59 +0900

    week03 day4

---
+++ algorithm/src/week3/day4/java/Pj_12925.java
@@ -0,0 +1,13 @@
+package week3.day4.java;
+
+public class Pj_12925 {
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Pj_12925 ss = new Pj_12925();
+        ss.solution("1234");
+    }
+}
