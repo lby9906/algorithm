21bab29618d8fd7f6ca101d2d6d974d1067b8c85
lby9906 <lby9906@naver.com>
2025-08-12 09:34:18 +0900

    week07 day2

---
+++ algorithm/src/week7/day2/java/Pj_77884.java
@@ -0,0 +1,29 @@
+package week7.day2.java;
+
+public class Pj_77884 {
+    public int solution(int left, int right) {
+        int result = 0;
+
+        for (int i=left; i<=right; i++) {
+            int count = 0;
+            for (int j=1; j<=i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0) {
+                result += i;
+            }else {
+                result -= i;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Pj_77884 ss = new Pj_77884();
+        ss.solution(13, 17);
+    }
+}
