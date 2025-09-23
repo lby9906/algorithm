d8bc2c0ac1b87063bcbc5fae49deca6ef0db0aa2
lby9906 <lby9906@naver.com>
2025-09-23 09:30:03 +0900

    week13 day2

---
+++ algorithm/src/week13/day2/java/Bj_18108.java
@@ -0,0 +1,14 @@
+package week13.day2.java;
+
+import java.util.Scanner;
+
+public class Bj_18108 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int inputYear = in.nextInt();
+
+		int num = inputYear - 544;
+		System.out.println(num);
+	}
+}
